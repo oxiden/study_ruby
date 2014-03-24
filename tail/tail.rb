60368f76304ba5ac09f3857b5f406c3940587e5e
OXIDEN <oxiden.dd@gmail.com>
2014-03-24 21:37:18 +0900

    add: tail.rb, tail2.rb

---
+++ tail/tail.rb
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+# ファイルの終端３行を表示するプログラム
+#
+# 実行例) ruby tail.rb /var/log/messages
+#
+if ARGV.size == 1
+  filename = ARGV[0]
+  # ファイルを開く
+	file = File.open(filename)
+  # いったんファイル内容をすべて読み出す
+  payload = file.read
+  # ファイル内容をLF(改行コード)文字で分割する(分割結果がArrayクラスのオブジェクトになる)
+  splitted_payload = payload.split("\n")
+  # ファイル行数＝Arrayの要素数、なので要素数を取得しておく。
+  line_number = splitted_payload.count
+  # ファイル内容(Array)を1行(1要素)ずつ取得
+  i = 1
+	splitted_payload.each do |x|
+    if i > line_number - 3 # 行数 が ファイル行数-3 以降なら行を表示
+      puts x
+    end
+    i = i + 1
+  end
+  # ファイルを閉じる
+  file.close
+end
---
+++ tail/tail2.rb
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+# ファイルの終端３行を表示するプログラム(改良版)
+#
+# 実行例) ruby tail2.rb /var/log/messages
+#
+
+# ポイント1：Fileクラスのopenメソッドをブロック付きバージョンで使用し、closeメソッドを不要にする。
+# ポイント2：再利用しない一時的な変数を省略しメモリを節約。
+#            file.readで得られたStringクラスのオブジェクトを、Stringクラスのsplitメソッドで改行
+#            ごとに分割しArrayクラスのオブジェクトに変換。
+#            Arrayクラスの[始点, 要素数]指定で終端3要素から3個(つまり終端3要素だけ)を取得
+#            終端3要素(Array)をeachでループさせ、表示させる。
+#
+
+case ARGV.size
+when 1
+	File.open(ARGV[0]) do |file| # ポイント1
+    file.read.split("\n")[-3, 3].each do |x| # ポイント2
+      puts x
+    end
+  end
+else
+  puts "ERROR: オプションが正しくありません。"
+end
